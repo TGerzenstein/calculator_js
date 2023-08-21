7eef17e4b4bf9c86028643eabf2f0b305eb80cac
TGerzenstein <tatigerzens@gmail.com>
2023-08-21 09:03:16 -0300

    Added js my project

--- app.js
+++ app.js
@@ -2,10 +2,41 @@ const screen = document.querySelector(".screen");
 const buttons = document.querySelectorAll(".btn");
 
 
+
+
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-       const buttonPush = button.textContent;
+      const buttonPush = button.textContent;
+      
+      
+      if (button.id === "c") {
+        screen.textContent = "0";
+        return;
+      }
+      
 
-       screen.textContent += buttonPush;
-    })
+      if (button.id === "delete") {
+        if (screen.textContent.length === 1 || screen.textContent === "Error!" ) {
+          screen.textContent = "0";
+        } else {
+          screen.textContent = screen.textContent.slice(0, -1);
+        }
+        return;
+      }
+
+      if (button.id === "equal") {
+        try {
+          screen.textContent = eval(screen.textContent);
+        } catch {
+          screen.textContent = "Error!";
+        }
+        return;
+      }
+
+      if (screen.textContent === "0" || screen.textContent === "Error!") {
+        screen.textContent = buttonPush;
+      } else {
+        screen.textContent += buttonPush;
+      }
+  })
 })
